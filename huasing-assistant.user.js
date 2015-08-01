250ee60fc55b1eaf55afb82ba223850950953cc1
julia2015 <julia@dispostable.com>
2015-08-01 11:35:03 +0800

    .parent().parent() 强迫症发作 suggestion by ricky8631

--- huasing-assistant.user.js
+++ huasing-assistant.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        和谐华新助手
 // @include     http://bbs.huasing.org/*
-// @version     0.2
+// @version     0.21
 // @description Hide posts from penzi
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @grant       GM_addStyle
@@ -13,28 +13,28 @@ var penzi = ['唐甬', 'materialist']
 // hide post in 华新鲜事
 $('div.brow2 div[class="ccol6 brow2h"] a[class="nickname bblack"]').each(function(index, element) {
     if ($.inArray($(this).attr('title'), penzi) > -1) {
-        $(this).parent().parent().parent().hide();
+        $(this).parents().eq(2).hide();
     }
 });
 
 // hide post in 版面
 $('div.brow2 div[class="bcol5 brow2h"] a[class="nickname bblack"]').each(function(index, element) {
     if ($.inArray($(this).attr('title'), penzi) > -1) {
-        $(this).parent().parent().parent().hide();
+        $(this).parents().eq(2).hide();
     }
 });
 
 // hide 在帖子里的
 $('div.authordiv div:first-child>a:first-child').each(function(index, element) {
     if ($.inArray($(this).text(), penzi) > -1) {
-        $(this).parent().parent().hide();
-        $(this).parent().parent().next().hide();
+        $(this).parents().eq(1).hide();
+        $(this).parents().eq(1).next().hide();
     }
 });
 
 // hide 漂浮出来的frame里的
 $('div[id^="i-"]>div a[href^="user.php"]>font>b').each(function(index, element) {
     if ($.inArray($(this).text(), penzi) > -1) {
-        $(this).parent().parent().parent().parent().hide();
+        $(this).parents().eq(3).hide();
     }
 });
