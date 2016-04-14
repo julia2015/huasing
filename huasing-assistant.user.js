9d258bc060663326c428fdc8ccc71eecc687e5e9
oglop <oglops@gmail.com>
2016-04-13 23:40:13 -0700

    add wap support

--- huasing-assistant.user.js
+++ huasing-assistant.user.js
@@ -1,14 +1,16 @@
 // ==UserScript==
 // @name        和谐华新助手
 // @include     http://bbs.huasing.org/*
-// @version     0.21
+// @version     0.22
 // @description Hide posts from penzi
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js
 // @grant       GM_addStyle
 // ==/UserScript==
 
 // 可以自己加的 ^_^ 我这是测试数据而已 不是攻击啊，别喷我
 var penzi = ['唐甬', 'materialist']
+
+window.jQuery = window.$ = jQuery
 
 // hide post in 华新鲜事
 $('div.brow2 div[class="ccol6 brow2h"] a[class="nickname bblack"]').each(function(index, element) {
@@ -36,5 +38,28 @@ $('div.authordiv div:first-child>a:first-child').each(function(index, element) {
 $('div[id^="i-"]>div a[href^="user.php"]>font>b').each(function(index, element) {
     if ($.inArray($(this).text(), penzi) > -1) {
         $(this).parents().eq(3).hide();
+    }
+});
+
+// --------------------- wap ---------------------
+
+// hide post in 华新鲜事
+$('.list>ul>li>em>a:nth-child(2)').each(function(index, element) {
+    if ($.inArray($(this).text(), penzi) > -1) {
+        $(this).parents().eq(1).hide();
+    }
+});
+
+// hide 在帖子里的
+$('p>em>a:nth-child(1)').each(function(index, element) {
+    if ($.inArray($(this).text(), penzi) > -1) {
+        $(this).parents().eq(1).hide();
+    }
+});
+
+// hide 首页
+$('.con2>ul>li>a:nth-child(3)').each(function(index, element) {
+    if ($.inArray($(this).text(), penzi) > -1) {
+        $(this).parents().eq(0).hide();
     }
 });
